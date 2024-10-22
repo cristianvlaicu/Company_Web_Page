4a34cbfc96c878adfcf78b0a050321968364c120
Cristian Vlaicu <cristian_fly90@yahoo.es>
2024-10-23 00:36:08 +0200

    Update send email sensitive info.

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "lordbeilish13@gmail.com"
-    password = "iklyrqycfxegxogp"
+    password = os.getenv("password")
     receiver = "dkelnumero1@gmail.com"
     context = ssl.create_default_context()
 
